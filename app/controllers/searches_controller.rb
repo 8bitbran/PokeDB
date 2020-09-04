ef94989770ae549e26c155d7b2923b49eafceb27
8bitbran <brandonmar97@gmail.com>
2020-09-03 20:42:30 -0400

    Added new action to searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,4 +2,10 @@ class SearchesController < ApplicationController
     def show
         @search = Search.find(params[:id])
     end 
+
+    def new 
+        @search = Search.new
+        @types = Pokemon.distinct.pluck(:type)
+        @regions = Pokemon.distinct.pluck(:region)
+    end
 end
