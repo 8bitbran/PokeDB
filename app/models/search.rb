930ca0a42f1e50ef17b4fee564e7f5eb75b31328
8bitbran <brandonmar97@gmail.com>
2020-09-03 20:53:08 -0400

    Updated search_pokemon method to check name, type, region

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,6 +4,10 @@ class Search < ApplicationRecord
     def search_pokemon
         pokemon = Pokemon.all 
 
+        pokemon = pokemon.where(['name LIKE ?', name]) if name.present?
+        pokemon = pokemon.where(['type LIKE ?', type]) if type.present?
+        pokemon = pokemon.where(['region LIKE ?', region]) if region.present?
+
         return pokemon
     end
 end
